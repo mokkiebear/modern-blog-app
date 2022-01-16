a61ceff09fdd13b5fba6ebaad7188e9042a6df90
mokkiebear <mirubanov@gmail.com>
2022-01-16 16:47:53 +0300

    rename blog

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Название - Блог за полдня!)
+              I am here - выражай свое мнение здесь!
             </span>
           </Link>
         </div>
